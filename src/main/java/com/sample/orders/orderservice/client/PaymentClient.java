298b6885893e344eddc731c1236419e63b47078c
VishwaVaranasi <vishweswara.varanasi@oracle.com>
2025-09-21 18:15:38 +0530

    updated fiegn client

--- src/main/java/com/sample/orders/orderservice/client/PaymentClient.java
+++ src/main/java/com/sample/orders/orderservice/client/PaymentClient.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "payment-service", url = "http://localhost:8082")
+@FeignClient(name = "payment-service", url = "http://localhost:8084")
 public interface PaymentClient {
-    @PostMapping("/payments/process")
+    @PostMapping("/api/v1/payments/process")
     PaymentResponse processPayment(@RequestBody PaymentRequest request);
 }
 
